1958b44a423b76641a7cd6d2b9d53f8039af02d2
Max Scott <maxscoottgames@gmail.com>
2026-01-08 17:24:43 +0300

    functions

---
+++ Functions and methods/Functions and methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Functions_and_methods
+{
+
+    class Program
+    {
+        static int Sum(int a, int b)
+        { 
+            return a + b;
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine("Sum result: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = Sum(a, b);
+
+            PrintResult(c);
+        }
+    }
+}
